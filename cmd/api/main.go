170c8a1aabe0f4a2c61c7f56a8f99781f4ce0e2d
Giwa Ezekiel <ezekielgiwa35@gmail.com>
2026-09-19 14:40:05 +0100

    feat: add server, resonse and router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,10 +2,19 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/zekielmp/Bitly/internal/config"
 	"github.com/zekielmp/Bitly/internal/database"
 	"github.com/zekielmp/Bitly/internal/logger"
+	"github.com/zekielmp/Bitly/internal/server"
 )
 
 func main() {
@@ -35,5 +44,34 @@ func main() {
 
 	gin.SetMode(cfg.Server.GinMode)
 
-	log.Info().Msg("Starting server")
+	srv := server.New(cfg, db, log)
+
+	router := srv.SetupRoute()
+	httpServer := &http.Server{
+		Addr:         fmt.Sprintf(":%s", cfg.Server.Port),
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	go func() {
+		log.Info().Str("port", cfg.Server.Port).Msg("Starting http server")
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal().Err(err).Msg("failed to start server")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Info().Msg("Shurting down server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := httpServer.Shutdown(ctx); err != nil {
+		log.Fatal().Err(err).Msg("failed to shutdown http server")
+	}
+
+	log.Info().Msg("shutting down database")
+
 }
